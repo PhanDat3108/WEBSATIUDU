9e7cb1c256cf1b47489cf2156e293c5cd7655491
PhanDat3108 <phandat.ptit@gmail.com>
2025-12-08 17:14:24 +0700

    fix

--- pharmacy-management/src/api/loaiThuocApi.ts
+++ pharmacy-management/src/api/loaiThuocApi.ts
@@ -2,7 +2,7 @@
 import { LoaiThuoc } from '../interfaces';
 
 
-const API_BASE_URL = 'http://localhost:8080/api/v1/loaithuoc'; 
+const API_BASE_URL = '/api/v1/loaithuoc'; 
 
 /**
  * Hàm chung xử lý response
